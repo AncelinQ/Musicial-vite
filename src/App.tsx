f236a4826e1de8e334fe7cf53fe22903840d3180
Ancelin <57001956+AncelinQ@users.noreply.github.com>
2022-01-02 17:59:09 +0100

    added basis from former development #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bulma/css/bulma.min.css';
+import {
+  AdsPage,
+  AdsSinglePage,
+  BandsSinglePage,
+  HomePage,
+  LoginPage,
+  MusiciansSinglePage,
+} from './pages';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { StandardLayout } from './layouts';
+import MusiciansPage from './pages/musicians-page';
+import BandsPage from './pages/bands-page';
 
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <StandardLayout>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/ads' element={<AdsPage />} />
+          <Route path='/ads/:id' element={<AdsSinglePage />} />
+          <Route path='/bands' element={<BandsPage />} />
+          <Route path='/bands/:id' element={<BandsSinglePage />} />
+          <Route path='/musicians' element={<MusiciansPage />} />
+          <Route path='/musicians/:id' element={<MusiciansSinglePage />} />
+        </Routes>
+      </Router>
+    </StandardLayout>
+  );
+};
 
-export default App
+export default App;
